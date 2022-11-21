d7832d3452999cdbf74643091595d9a219277f34
Mohamad Sahyouni <sahyounm@hochschule-trier.de>
2022-11-21 19:57:50 +0100

    upload v1

---
+++ webapp/src/js/Main.mjs
@@ -0,0 +1,12 @@
+import { SingleWord, ThreeWords } from 'lib';
+
+SingleWord.print();
+ThreeWords.print();
+greet();
+alert();
+function alert () {
+  alert('hello');
+}
+function greet () {
+  console.log('Hello, World!');
+}
