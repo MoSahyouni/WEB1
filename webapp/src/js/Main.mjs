4c03c30610e938a49327bb3fe6f8cf39ff96fcfa
MoSahyouni <damdasahyouni@gmail.com>
2022-12-21 20:54:11 +0100

    21/12 routing anmelden

--- webapp/src/js/Main.mjs
+++ webapp/src/js/Main.mjs
@@ -1,16 +1,21 @@
 
-document.getElementById('testMain').innerHTML = `<h1> Hello World</h1> 
+document.getElementById('Main').innerHTML = `<h1> Hello World</h1> 
 <p>das ist Probetext!! </p> 
-<button id="anmelden",onclick="location.href='/anmelden'">anmelden</button>`;
+`;
 
 const main = document.getElementsByClassName('Main')[0];
 const btn = document.createElement('button');
-btn.textContent = 'click';
+btn.textContent = 'anmelden';
 btn.setAttribute('id', 'btn');
 btn.setAttribute('onclick', "location.href='/anmelden'");
 main.appendChild(btn);
 btn.addEventListener('click', () => {
-  const a = document.createElement('a');
+  /* const a = document.createElement('a');
   a.textContent = 'nee';
-  main.appendChild(a);
+  main.appendChild(a); */
 });
+const btn2 = document.createElement('button');
+btn2.textContent = 'neue Veranstaltung';
+btn2.setAttribute('id', 'btn2');
+btn2.setAttribute('onclick', "location.href='/neueVeranstaltung'");
+main.appendChild(btn2);
--- webapp/src/js/server.mjs
+++ webapp/src/js/server.mjs
@@ -11,11 +11,39 @@ if (process.argv.length >= 3) {
 
 server.use(express.static('dist'));
 server.get('/', (request, response) => {
-  response.send('ih');
+  response.send('hi');
 });
 
 server.get('/anmelden', (request, response) => {
-  response.send('Anmelden');
+  response.send(`
+  <ul id="list", style="color: rgb(85, 26, 1)">
+    <li>Vorname: <input id="vname", type="text"></li>
+    <li>Nachname: <input id="nname", type="text"></li>
+    <li>passwort: <input id="passw", type="text"></li>
+    <br>
+    <button id="btnJSON" >Nutzer erstellen</button>
+  </ul>
+  <p id="userdata"></p> 
+  <script>
+  const vname = document.getElementById('vname');
+  const nname = document.getElementById('nname');
+  const pass = document.getElementById('passw');
+  const btnJSN = document.getElementById('btnJSON');
+  const userdatatext = document.getElementById("userdata");
+  btnJSN.addEventListener("click", function(){
+    var user = {
+      "Vorname": vname.value,
+      "Nachname": nname.value,
+      "Passwort": pass.value
+    }
+    userdatatext.innerHTML= JSON.stringify(user);
+  })
+  </script>
+  `);
+});
+
+server.get('/neueVeranstaltung', (request, response) => {
+  response.send('Neue Veranstaltung anlegen');
 });
 
 server.get('/json', (request, response) => {
